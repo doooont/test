07d42166c35f404eb583132cfe5abe802d45373b
doooont <doooont@example.com>
2018-05-27 17:21:15 +0800

    test rpg

---
+++ Rpg/Rpg/Item.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rpg
+{
+    class Item
+    {
+
+    }
+    
+    class Equipment
+    {
+         struct Weapon
+        {
+          public string name { get; private set; }
+          public float atk { get; private set; }
+          public float speed { get; private set; }
+        }
+        struct Armor
+        {
+            public string name { get; private set; }
+            public float def { get; private set; }
+            public float speed { get; private set; }
+            public float hp { get; private set; }
+            public float mana { get; private set; }
+        }
+        struct Shoes
+        {
+            public string name { get; private set; }
+            public float def { get; private set; }
+            public float speed { get; private set; }
+            public float hp { get; private set; }
+            public float mana { get; private set; }
+        }
+        
+        List<Weapon> weaponlist = new List<Weapon>();
+        
+        public void GetWeapon()
+        {
+            Weapon w1= new Weapon();
+            w1.name = "木剑";
+        }
+
+
+
+
+    }
+}
---
+++ Rpg/Rpg/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rpg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    class PlayerState
+    {
+        public float hpMax { get; set; } 
+        public float HpCurrent
+        {
+            get { return hpCurrent; }
+            set
+            {
+                if (value >=hpMax)
+                {
+                    hpCurrent = hpMax;
+                }
+                else
+                {
+                    hpCurrent = value;
+                }
+            }
+        }
+
+        private float hpCurrent;
+        public float ATK { get; set; }
+        public float DEF { get; set; }
+        public float Mana { get; set; }
+        public float Manacurrent
+        {
+            get { return manacurrent; }
+            set
+            {
+                if (value >=Mana )
+                {
+                    manacurrent = Mana;
+                }
+                else
+                {
+                    manacurrent = value;
+                }
+            }
+        }
+
+        private float manacurrent;
+        public float speed { get; set; }
+        public PlayerState()
+        {
+            hpMax = 100;
+            hpCurrent = 100;
+            Mana = 50;
+            Manacurrent = 50;
+            ATK = 15;
+            DEF = 10;
+            speed = 8;
+        }
+
+
+    }
+}
