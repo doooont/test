c692cbbd48b45c9d2defa40aa4d8cac69579cb69
doooont <doooont@example.com>
2018-05-27 11:28:42 +0800

    jiandao shitou bu v1.0

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,27 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            
+            Player player = new Player();
+            Enemy enemy = new Enemy(player);
+            cc: player.GetMassage();
+            Console.WriteLine("player hp {0}    enemy hp {1}",player.hp,enemy.hp);
+            if (player .hp<=0)
+            {
+                Console.WriteLine("you lose the game");
+                Console.ReadKey();
+                return;
+                
+            }
+            if (enemy.hp<=0)
+            {
+                Console.WriteLine("you win the game");
+                Console.ReadKey();
+                return;
+            }
+            else
+            {
+                goto cc;
+            }
 
         }
     }
@@ -18,15 +38,16 @@ namespace ConsoleApp1
     class Player
     {
         public event ChoiceEventHandler choiceEvent;
-         int hp;
+        public int hp { get; set; }
         public Player()
         {
             hp = 100;
 
         }
-        public void  GetMassage(string choice)
+        public void  GetMassage()
         {
-            choice = Console.ReadKey().ToString();
+            Console.WriteLine("please writedown jiandao or shitou or bu");
+            string choice = Console.ReadLine().ToString();
             choiceEvent(choice);
             
         }
@@ -34,25 +55,68 @@ namespace ConsoleApp1
     }
     class Enemy
     {
-        int hp;
+        public int hp { get; set; }
         Player player;
+        List <string > clist;
         public Enemy(Player player)
         {
             hp = 100;
             this.player = player;
-            player .choiceEvent+=
+            clist = new List<string>() { "shitou", "jiandao", "bu" };
+            player.choiceEvent += Panduan;
         }
-        public void Panduan(string pchoice, string echoice)
+        public void Panduan(string pchoice)
         {
-            if (pchoice != echoice)
+            Random random = new Random();
+            string echoice= clist[random.Next(0, 3)];
+            if (pchoice==echoice )
             {
-
+                Console.WriteLine("pingju");
+                return;
+            }
+            if (pchoice=="shitou"&&echoice=="jiandao")
+            {
+                Console.WriteLine("you win");
+                hp -= 10;
+                return;
+            }
+            if (pchoice=="shitou"&&echoice=="bu")
+            {
+                Console.WriteLine("you lose");
+                player.hp -= 10;
+                return;
+            }
+            if (pchoice =="jiandao"&&echoice=="shitou")
+            {
+                Console.WriteLine("you lose");
+                player.hp -= 10;
+                return;
+            }
+            if (pchoice =="jiandao"&&echoice =="bu")
+            {
+                Console.WriteLine("you win");
+                hp -= 10;
+                return;
+            }
+            if (pchoice=="bu"&&echoice=="shitou")
+            {
+                Console.WriteLine("you win");
+                hp -= 10;
+                return;
+            }
+            if (pchoice=="bu"&&echoice =="jiandao")
+            {
+                Console.WriteLine("you lose");
+                player.hp -= 10;
+                return;
+            }
+            else
+            {
+                Console.WriteLine("error");
+                return;
             }
         }
-        string  EnemyChoice()
-        {
-
-        }
+        
     }
     
 }
